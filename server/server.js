f398bf572319c0dfb198bcdb8e7f0001996fc0e5
sapar6ek0v <razetka0309@gmail.com>
2022-02-07 18:38:00 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import path from "path";
+import testRoutes from "./routes/testRoutes.js";
+
+dotenv.config()
+
+const port = process.env.PORT || 8000
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.set('json spaces', 2)
+
+server.use('/api', testRoutes)
+
+server.use(express.static('client/build'))
+server.use("*", (req, res) => {
+    res.sendFile(path.resolve('client/build/index.html'))
+})
+
+server.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`)
+})
